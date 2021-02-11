594c9b1345ed22e336c08f3cbdd2550655437f5e
Alex <alex.hertel87@gmail.com>
2021-02-11 16:02:18 -0600

    Second change commit

---
+++ connect-four.js
@@ -0,0 +1,48 @@
+/*
+
+//* check if a column is full (all spaces filled in column) --> if it is full, get id for that column and change class to "click-target full"
+
+//* if P1 turn, indicate via black token, P2 is red token
+      //* change class on the div target to be color of current player
+
+//* putting a token in a square creates a div set to the color and append that div to the square element when it drops in (black or red)
+
+*/
+
+import {
+   Game
+} from "./game.js"
+
+let game = undefined
+function updateUI() {
+   let board = document.getElementById("board-holder")
+   let gameName = document.getElementsById("game-name")
+   if (game === undefined) {
+      board.classList.add("is-invisible")
+   } else {
+      board.classList.remove("is-invisible");
+      gameName.innerHTML = game.getName()
+   }
+}
+window.addEventListener("DOMContentLoaded", () => {
+   let newGameBtn = document.getElementById("new-game")
+   let playerOne = document.getElementById("player-1-name")
+   let playerTwo = document.getElementById("player-2-name")
+   playerOne.addEventListener("keyup", event => {
+      if (playerOne.value !== "") {
+         newGameBtn.disabled === false
+      } else { newGameBtn.disabled === true }
+   })
+   playerTwo.addEventListener("keyup", event => {
+      if(playerTwo.value !== "")
+})
+
+   newGameBtn.addEventListener("click", event => {
+      game = new Game(playerOne.value, playerTwo.value)
+      playerOne.value = ""
+      playerTwo.value = ""
+      newGameBtn.disabled === true
+      updateUI()
+   })
+
+})
---
+++ game.js
@@ -0,0 +1,13 @@
+class Game {
+   constructor(name1, name2) {
+      this.name1 = playerOneName
+      this.name2 = playerTwoName
+      this.getName()
+   }
+   getName() {
+      this.playerOneName = document.getElementById("player-1-name").value
+      this.playerTwoName = document.getElementById("player-2-name").value
+   }
+}
+
+export {Game}
