372981d26ecc33f49251602ed87582cf3cd42e37
Josh Steinberg <jpsteinberg@gmail.com>
2021-02-11 19:19:52 -0500

    working columns

--- connect-four.js
+++ connect-four.js
@@ -9,40 +9,54 @@
 
 */
 
-import {
-   Game
-} from "./game.js"
+import { Game } from "./game.js";
 
-let game = undefined
+let game = undefined;
 function updateUI() {
-   let board = document.getElementById("board-holder")
-   let gameName = document.getElementsById("game-name")
-   if (game === undefined) {
-      board.classList.add("is-invisible")
-   } else {
-      board.classList.remove("is-invisible");
-      gameName.innerHTML = game.getName()
-   }
+  let board = document.getElementById("board-holder");
+  let gameName = document.getElementById("game-name");
+  let clickTarget = document.getElementById("click-targets");
+  if (game === undefined) {
+    board.classList.add("is-invisible");
+  } else {
+    board.classList.remove("is-invisible");
+    gameName.innerHTML = game.getName();
+    if (game.currentPlayer === 1) {
+      clickTarget.classList.remove("red");
+      clickTarget.classList.add("black");
+    } else {
+      clickTarget.classList.remove("black");
+      clickTarget.classList.add("red");
+    }
+  }
 }
 window.addEventListener("DOMContentLoaded", () => {
-   let newGameBtn = document.getElementById("new-game")
-   let playerOne = document.getElementById("player-1-name")
-   let playerTwo = document.getElementById("player-2-name")
-   playerOne.addEventListener("keyup", event => {
-      if (playerOne.value !== "") {
-         newGameBtn.disabled === false
-      } else { newGameBtn.disabled === true }
-   })
-   playerTwo.addEventListener("keyup", event => {
-      if(playerTwo.value !== "")
-})
+  let newGameBtn = document.getElementById("new-game");
+  let playerOne = document.getElementById("player-1-name");
+  let playerTwo = document.getElementById("player-2-name");
+  playerOne.addEventListener("keyup", (event) => {
+    if (playerOne.value !== "") {
+      newGameBtn.disabled = false;
+    } else {
+      newGameBtn.disabled = true;
+    }
+  });
+  playerTwo.addEventListener("keyup", (event) => {
+    if (playerTwo.value !== "") {
+    }
+  });
 
-   newGameBtn.addEventListener("click", event => {
-      game = new Game(playerOne.value, playerTwo.value)
-      playerOne.value = ""
-      playerTwo.value = ""
-      newGameBtn.disabled === true
-      updateUI()
-   })
+  newGameBtn.addEventListener("click", (event) => {
+    game = new Game(playerOne.value, playerTwo.value);
+    playerOne.value = "";
+    playerTwo.value = "";
+    newGameBtn.disabled = true;
+    updateUI();
+  });
 
-})
+  let clickTarget = document.getElementById("click-targets");
+  clickTarget.addEventListener("click", (event) => {
+    game.playInColumn();
+    updateUI();
+  });
+});
--- game.js
+++ game.js
@@ -1,13 +1,26 @@
 class Game {
-   constructor(name1, name2) {
-      this.name1 = playerOneName
-      this.name2 = playerTwoName
-      this.getName()
-   }
-   getName() {
-      this.playerOneName = document.getElementById("player-1-name").value
-      this.playerTwoName = document.getElementById("player-2-name").value
-   }
+  constructor(name1, name2) {
+    this.name1 = name1;
+    this.name2 = name2;
+    this.getName();
+    this.currentPlayer = 1;
+  }
+
+  getName() {
+    return `${this.name1} vs. ${this.name2}`;
+  }
+
+  playInColumn() {
+    //  let clickTarget = document.getElementById("click-targets");
+    //  clickTarget.addEventListener("click", (event) => {
+    if (this.currentPlayer === 1) {
+      this.currentPlayer = 2;
+    } else {
+      this.currentPlayer = 1;
+    }
+    //     });
+    //   }
+  }
 }
 
-export {Game}
+export { Game };
